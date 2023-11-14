270eed38aab0326468c55a6bc451883e0d50ef7f
nielrya4 <nielrya4@isu.edu>
2023-11-14 11:13:34 -0700

    Added file upload capabilities

--- app.py
+++ app.py
@@ -1,73 +1,99 @@
 import base64
-import numpy as np
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from werkzeug.utils import secure_filename
 from io import BytesIO
-from flask import Flask, render_template, request
 from matplotlib.figure import Figure
+import openpyxl
+import numpy as np
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    # Your data and bandwidth (sigma)
-    data = [1,1,2,3,4,5,6,7,8,9]    # Replace with your actual data
-    sigma = 1                       # Replace with your desired bandwidth
-    result = plotKDE(data, sigma)
-    return result
+def read_excel(file):
+    data = []
+    sigma = []
+
+    try:
+        wb = openpyxl.load_workbook(file, read_only=True)
+        sheet = wb.active
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            data.append(row[0])
+            sigma.append(row[1])
+    except Exception as e:
+        raise ValueError(f"Error reading Excel file: {e}")
+
+    return data, sigma
 
 def plotKDE(data, sigma):
-    # Generate the figure **without using pyplot**.
+    # Generate the figure without using pyplot.
     fig = Figure()
     plt = fig.subplots()
+
     # Call the kernel_density_estimation function to get the KDE result
     kde_result = kernel_density_estimation(data, sigma)
+
     # Extract X and Y values from the result
     x_values = kde_result[:, 0]
     y_values = kde_result[:, 1]
-#
+
     # Create a KDE plot using Matplotlib
     plt.plot(x_values, y_values, label='KDE')
-    #plt.xlabel('X')
-    #plt.ylabel('Density')
-    #plt.title('Kernel Density Estimation (KDE) Plot')
     plt.legend()
-    # Save it to a temporary buffer.
+
+    # Save the plot to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
-    # Embed the result in the html output.
+
+    # Embed the result in the HTML output.
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return f"<img src='data:image/png;base64,{data}'/>"
 
 def kernel_density_estimation(data, sigma, nsteps=100):
     result = np.zeros((nsteps, 2))
-    x = np.zeros(nsteps)
+    x = np.linspace(min(data) - 2 * max(sigma), max(data) + 2 * max(sigma), nsteps)
     y = np.zeros(nsteps)
 
-    MAX = float('-inf')
-    MIN = float('inf')
     N = len(data)  # number of data points
 
-    # Find MIN MAX values in data
-    for i in range(N):
-        if MAX < data[i]:
-            MAX = data[i]
-        if MIN > data[i]:
-            MIN = data[i]
-
-    # Like MATLAB linspace(MIN, MAX, nsteps);
-    x[0] = MIN
-    for i in range(1, nsteps):
-        x[i] = x[i - 1] + ((MAX - MIN) / nsteps)
-
     # Kernel density estimation
-    c = 1.0 / (np.sqrt(2 * np.pi * sigma**2))
     for i in range(N):
-        for j in range(nsteps):
-            y[j] = y[j] + 1.0 / N * c * np.exp(-((data[i] - x[j])**2) / (2 * sigma**2))
+        y = y + 1.0 / N * (1.0 / (np.sqrt(2 * np.pi) * sigma[i])) * np.exp(-(x - data[i])**2 / (2 * sigma[i]**2))
 
-    # Compilation of the X,Y to result. Good for creating plot(x, y)
-    for i in range(nsteps):
-        result[i, 0] = x[i]
-        result[i, 1] = y[i]
+    # Compilation of the X, Y to result. Good for creating plot(x, y)
+    result[:, 0] = x
+    result[:, 1] = y
+
     return result
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
 
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            try:
+                data, sigma = read_excel(file)
+                result = plotKDE(data, sigma)
+                return result
+            except ValueError as e:
+                flash(str(e))
+                return redirect(request.url)
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
