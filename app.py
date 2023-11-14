cb061949102dfbd97b1ca54055a733c67de7f178
nielrya4 <nielrya4@isu.edu>
2023-11-14 13:20:27 -0700

    Added image download options for svg and png

--- app.py
+++ app.py
@@ -1,77 +1,25 @@
 import base64
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, send_file, Response, session
 from werkzeug.utils import secure_filename
 from io import BytesIO
 from matplotlib.figure import Figure
 import openpyxl
 import numpy as np
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
+import secrets
+
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def read_excel(file):
-    data = []
-    sigma = []
-
-    try:
-        wb = openpyxl.load_workbook(file, read_only=True)
-        sheet = wb.active
-        for row in sheet.iter_rows(min_row=2, values_only=True):
-            data.append(row[0])
-            sigma.append(row[1])
-    except Exception as e:
-        raise ValueError(f"Error reading Excel file: {e}")
-
-    return data, sigma
-
-def plotKDE(data, sigma):
-    # Generate the figure without using pyplot.
-    fig = Figure()
-    plt = fig.subplots()
-
-    # Call the kernel_density_estimation function to get the KDE result
-    kde_result = kernel_density_estimation(data, sigma)
-
-    # Extract X and Y values from the result
-    x_values = kde_result[:, 0]
-    y_values = kde_result[:, 1]
-
-    # Create a KDE plot using Matplotlib
-    plt.plot(x_values, y_values, label='KDE')
-    plt.legend()
-
-    # Save the plot to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-
-    # Embed the result in the HTML output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
-
-def kernel_density_estimation(data, sigma, nsteps=100):
-    result = np.zeros((nsteps, 2))
-    x = np.linspace(min(data) - 2 * max(sigma), max(data) + 2 * max(sigma), nsteps)
-    y = np.zeros(nsteps)
-
-    N = len(data)  # number of data points
-
-    # Kernel density estimation
-    for i in range(N):
-        y = y + 1.0 / N * (1.0 / (np.sqrt(2 * np.pi) * sigma[i])) * np.exp(-(x - data[i])**2 / (2 * sigma[i]**2))
-
-    # Compilation of the X, Y to result. Good for creating plot(x, y)
-    result[:, 0] = x
-    result[:, 1] = y
-
-    return result
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def main():
+    graph_data = None
+
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
@@ -86,14 +34,99 @@ def upload_file():
         if file and allowed_file(file.filename):
             try:
                 data, sigma = read_excel(file)
-                result = plotKDE(data, sigma)
-                return result
+                session['data'] = data  # Store data in the session
+                session['sigma'] = sigma  # Store sigma in the session
+                graph_data = plotKDE(data, sigma)
             except ValueError as e:
                 flash(str(e))
                 return redirect(request.url)
 
-    return render_template('index.html')
+    return render_template('index.html', graph_data=graph_data)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def read_excel(file):
+    data = []
+    sigma = []
+    try:
+        wb = openpyxl.load_workbook(file, read_only=True)
+        sheet = wb.active
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            data.append(row[0])
+            sigma.append(row[1])
+    except Exception as e:
+        raise ValueError(f"Error reading Excel file: {e}")
+    return data, sigma
+
+
+def plotKDE(data, sigma):
+    fig = Figure(figsize=(8, 6), dpi=100)                                              #Build the graph off of matplotlib's Figure
+    plt = fig.subplots()
+    kde_result = kernel_density_estimation(data, sigma)         #Calculate the (x,y) points from our data and uncertainty
+    x_values = kde_result[:, 0]                                 #Separate x and y values
+    y_values = kde_result[:, 1]
+    plt.plot(x_values, y_values, label='KDE')                   #Plot our graph
+    plt.legend()                                                #Make it have a legend
+    buf = BytesIO()
+    fig.savefig(buf, format="svg", bbox_inches="tight")  # Save the figure as SVG
+    data = buf.getvalue().decode("utf-8")
+    return f"<div>{data}</div>"
+
+
+def download_image_from_session(format, filename):
+    data = session.get('data', [])
+    sigma = session.get('sigma', 1)
+
+    fig = Figure(figsize=(8, 6), dpi=300)
+    plt = fig.subplots()
+    kde_result = kernel_density_estimation(data, sigma)
+    x_values = kde_result[:, 0]
+    y_values = kde_result[:, 1]
+    plt.plot(x_values, y_values, label='KDE')
+    plt.legend()
+    buf = BytesIO()
+    fig.savefig(buf, format=format, bbox_inches="tight")
+    buf.seek(0)
+    return Response(buf, mimetype=f'image/{format}', headers={'Content-Disposition': f'attachment;filename={filename}'})
+
+
+@app.route('/download', methods=['GET'])
+def download_plot():
+    format = request.args.get('format', 'png')
+
+    if format == 'png':
+        return download_image_from_session(format='png', filename='plot.png')
+    elif format == 'svg':
+        return download_image_from_session(format='svg', filename='plot.svg')
+    else:
+        flash('Invalid download format')
+        return redirect(url_for('upload_file'))
+
+def kernel_density_estimation(data, sigma, nsteps=1000):
+    result = np.zeros((nsteps, 2))
+    x = np.linspace(min(data) - 2 * max(sigma),
+                    max(data) + 2 * max(sigma),
+                    nsteps)
+    y = np.zeros(nsteps)
+    N = len(data)  # number of data points
+
+    # Check if sigma is a single value and convert it to a list
+    sigma = [sigma] if not isinstance(sigma, (list, tuple, np.ndarray)) else sigma
+
+    for i in range(N):
+        for s in sigma:
+            y = (y + 1.0 / N *
+                 (1.0 / (np.sqrt(2 * np.pi) * s)) *
+                 np.exp(-(x - data[i]) ** 2 / (2 * s ** 2)))
+
+    result[:, 0] = x
+    result[:, 1] = y
+    return result
+
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
