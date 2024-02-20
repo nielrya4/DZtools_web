Little cosmetic changes Fixed negative bandwidth bug Fixed pyplot data leak bug by closing plots

objects/graphs.py
@@ -128,7 +128,7 @@ class PDP:
 
 
 class MDS:
-    def __init__(self, samples, title):
+    def __init__(self, samples, title=None):
         self.samples = samples
         self.title = title
 
@@ -136,34 +136,25 @@ class MDS:
         samples = self.samples
         num_samples = len(samples)
         dissimilarity_matrix = np.zeros((num_samples, num_samples))
-        sample_names = [sample.name for sample in samples]  # Extract sample names
+        sample_names = [sample.name for sample in samples]
 
         for i in range(num_samples):
-            for j in range(i+1, num_samples):
+            for j in range(i + 1, num_samples):
                 dissimilarity_matrix[i, j] = measures.dissimilarity_test(samples[i], samples[j])
                 dissimilarity_matrix[j, i] = dissimilarity_matrix[i, j]
 
-        # Classical MDS algorithm
-        n = dissimilarity_matrix.shape[0]
-        H = np.eye(n) - np.ones((n, n)) / n
-        B = -0.5 * np.dot(np.dot(H, dissimilarity_matrix ** 2), H)
+        embedding = MultidimensionalScaling(n_components=2, dissimilarity='precomputed')
+        scaled_mds_result = embedding.fit_transform(dissimilarity_matrix)
 
-        eigenvalues, eigenvectors = np.linalg.eigh(B)
-
-        # Sort eigenvalues and eigenvectors in descending order
-        indices = np.argsort(eigenvalues)[::-1]
-        eigenvalues = eigenvalues[indices]
-        eigenvectors = eigenvectors[:, indices]
-
-        # Select the top two eigenvectors
-        scaled_mds_result = np.sqrt(eigenvalues[:2]) * eigenvectors[:, :2]
-
-        # Plotting
+        viridis = plt.cm.get_cmap('gist_ncar', num_samples)
+        colors = viridis(np.linspace(0, 1, num_samples))
         fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-        scatter = ax.scatter(scaled_mds_result[:, 0], scaled_mds_result[:, 1])
+        for i, (x, y) in enumerate(scaled_mds_result):
+            ax.scatter(x, y, color=colors[i])
+            ax.text(x, y + 0.005, sample_names[i], fontsize=8, ha='center', va='center')
 
         for i, (x, y) in enumerate(scaled_mds_result):
-            ax.text(x, y + 0.005, sample_names[i], fontsize=8, ha='center', va='center')  # Adjusted placement
+            ax.text(x, y + 0.005, sample_names[i], fontsize=8, ha='center', va='center')
 
         for i in range(num_samples):
             distance = float('inf')  # Initialize distance to positive infinity
@@ -181,7 +172,9 @@ class MDS:
                 x2, y2 = scaled_mds_result[samples.index(nearest_sample)]
                 ax.plot([x1, x2], [y1, y2], 'k--', linewidth=0.5)
 
-        fig.suptitle(self.title if self.title is not None else "Multidimensional Scaling Plot")
+        stress = embedding.stress_
+
+        fig.suptitle(self.title if self.title is not None else f"Multidimensional Scaling Plot (Stress: {np.round(stress, decimals=6)})")
         fig.text(0.5, 0.01, 'Dimension 1', ha='center', va='center', fontsize=12)
         fig.text(0.01, 0.5, 'Dimension 2', va='center', rotation='vertical', fontsize=12)
         fig.tight_layout()

utils/kde_utils.py
@@ -1,19 +1,19 @@
 import numpy as np
+from sklearn.neighbors import KernelDensity
 
 
 def kde_function(sample, num_steps=1000, x_min=0, x_max=4000):
-    x = np.linspace(x_min, x_max, num_steps)
-    y = np.zeros_like(x)
-    ages = [grain.age for grain in sample.grains]
-    bandwidths = [grain.uncertainty for grain in sample.grains]
-    for i in range(len(ages)):
-        kernel_sum = np.zeros(num_steps)
-        s = bandwidths[i]
-        kernel_sum += (1.0 / (np.sqrt(2 * np.pi) * s)) * np.exp(-(x - float(ages[i])) ** 2 / (2 * float(s) ** 2))
-        kernel_sum /= np.sum(kernel_sum)
-        y += kernel_sum
-    y /= np.sum(y)
-    return x, y
+    x = np.linspace(x_min, x_max, num_steps).reshape(-1, 1)
+    ages = np.array([grain.age for grain in sample.grains]).reshape(-1, 1)
+    bandwidths = np.array([np.abs(grain.uncertainty) for grain in sample.grains])
+
+    kde = KernelDensity(bandwidth=np.mean(bandwidths), kernel='gaussian')
+    kde.fit(ages)
+
+    log_dens = kde.score_samples(x)
+    y = np.exp(log_dens)
+
+    return x.flatten(), y / np.sum(y)
 
 
 def get_y_values(sample):

utils/unmixing_utils.py
@@ -18,8 +18,8 @@ def do_monte_carlo(samples, num_trials=10000):
     top_trials = get_percent_of_array(sorted_trials, 1)
     random_configurations = [trial.random_configuration for trial in top_trials]
 
-    source_contributions = np.average(random_configurations, axis=0)
-    source_std = np.std(random_configurations, axis=0)
+    source_contributions = np.average(random_configurations, axis=0)*100
+    source_std = np.std(random_configurations, axis=0)*100
 
     contribution_table_d = build_contribution_table(source_samples, source_contributions, source_std, test_type="KS")
     return contribution_table_d, None, None